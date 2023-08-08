89b4c52ca7796b4b9ca33403a4d219d8b931b180
Ana <anadimoska06@gmail.com>
2023-08-08 12:32:45 +0100

    broken code for testing func

---
+++ pkg/testServer/serverTesting/server_test.go
@@ -0,0 +1,58 @@
+package serverTesting
+
+import (
+	agent1 "basePlatformSOMAS/pkg/agents/AgentTesting/agent1"
+	agent2 "basePlatformSOMAS/pkg/agents/AgentTesting/agent2"
+	baseUserAgent "basePlatformSOMAS/pkg/agents/AgentTesting/baseuseragent"
+	baseAgent "basePlatformSOMAS/pkg/agents/BaseAgent"
+	infra "basePlatformSOMAS/pkg/infra/server"
+	testserver "basePlatformSOMAS/pkg/testServer"
+	"fmt"
+	"testing"
+)
+
+func main(t *testing.T) {
+
+	fmt.Println("Running base server:")
+	makeServerBase()
+	fmt.Println("Base server finished.")
+
+	fmt.Println("----------------------------")
+
+	fmt.Println("Running test server:")
+	makeServerTest()
+	fmt.Println("Test server finished.")
+
+}
+
+func makeServerBase(t *testing.T) {
+	m := make([]infra.AgentGeneratorCountPair[baseAgent.Agent], 1)
+	m[0] = infra.MakeAgentGeneratorCountPair(baseAgent.GetAgent, 4)
+
+	serv := infra.CreateServer[baseAgent.Agent](m, 5)
+
+	for _, agent := range serv.Agents {
+		if agent.GetID().(*baseagent.BaseAgent) != 0 {
+			t.Error("Error creating agent")
+
+		}
+		
+	}
+
+	t.Error("Error")
+
+	serv.RunGameLoop()
+	serv.Start()
+
+}
+
+func makeServerTest() {
+
+	m := make([]infra.AgentGeneratorCountPair[baseUserAgent.AgentUserInterface], 2)
+	m[0] = infra.MakeAgentGeneratorCountPair[baseUserAgent.AgentUserInterface](agent2.GetAgent, 3)
+	m[1] = infra.MakeAgentGeneratorCountPair[baseUserAgent.AgentUserInterface](agent1.GetAgent, 2)
+	floors := 3
+	ts := testserver.New(m, floors)
+	ts.RunGameLoop()
+	ts.Start()
+}
