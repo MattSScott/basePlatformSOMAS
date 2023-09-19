3fe815669b745738f014823909412a72c4cf1943
Matt Scott <matthew.scott3142@gmail.com>
2023-09-19 23:10:23 +0100

    added test to messaging package

--- pkg/messaging/message_test.go
+++ pkg/messaging/message_test.go
@@ -16,11 +16,21 @@ type Message2 struct {
 	messageField2 int
 }
 
+type NullMessage struct {
+	message.BaseMessage
+}
+
 type IExtendedAgent interface {
 	message.IAgentMessenger
+	GetAgentField() int
 	GetAllMessages() []message.IMessage
 	HandleMessage1(msg Message1)
 	HandleMessage2(msg Message2)
+	HandleNullMessage(msg NullMessage)
+}
+
+type ExtendedAgent struct {
+	agentField int
 }
 
 func (m1 Message1) Accept(agent message.IAgentMessenger) {
@@ -31,8 +41,12 @@ func (m2 Message2) Accept(agent message.IAgentMessenger) {
 	agent.(IExtendedAgent).HandleMessage2(m2)
 }
 
-type ExtendedAgent struct {
-	agentField int
+func (nm NullMessage) Accept(agent message.IAgentMessenger) {
+	agent.(IExtendedAgent).HandleNullMessage(nm)
+}
+
+func (ea *ExtendedAgent) GetAgentField() int {
+	return ea.agentField
 }
 
 func (ea *ExtendedAgent) GetMessage1() Message1 {
@@ -53,17 +67,24 @@ func (ea *ExtendedAgent) GetMessage2() Message2 {
 	}
 }
 
+func (ea *ExtendedAgent) HandleMessage2(msg Message2) {
+	ea.agentField += msg.messageField2
+}
+func (ea *ExtendedAgent) GetNullMessage(recips []message.IAgentMessenger) NullMessage {
+	return NullMessage{message.CreateMessage(ea, recips)}
+}
+
+func (ea *ExtendedAgent) HandleNullMessage(msg NullMessage) {
+	sender := msg.GetSender().(IExtendedAgent)
+	ea.agentField = sender.GetAgentField()
+}
+
 func (ea *ExtendedAgent) GetAllMessages() []message.IMessage {
 	msg1 := ea.GetMessage1()
 	msg2 := ea.GetMessage2()
 	return []message.IMessage{msg1, msg2}
 }
-
-func (ea *ExtendedAgent) HandleMessage2(msg Message2) {
-	ea.agentField += msg.messageField2
-}
-
-func TestMessageClass(t *testing.T) {
+func TestMessageCanBeExtended(t *testing.T) {
 	agent1 := ExtendedAgent{agentField: 0}
 	agent2 := ExtendedAgent{agentField: 0}
 
@@ -79,6 +100,23 @@ func TestMessageClass(t *testing.T) {
 
 	if agent2.agentField != 5 {
 		t.Error("Message 1 not properly handled")
+	}
+}
+
+func TestMessageSender(t *testing.T) {
+
+	a1 := &ExtendedAgent{agentField: 5}
+	a2 := &ExtendedAgent{agentField: 10}
+	a3 := &ExtendedAgent{agentField: 15}
+
+	nullMsg := a1.GetNullMessage([]message.IAgentMessenger{a2, a3})
+
+	for _, recip := range nullMsg.GetRecipients() {
+		nullMsg.Accept(recip)
+	}
+
+	if a1.GetAgentField() != a2.GetAgentField() || a3.GetAgentField() != a1.GetAgentField() {
+		t.Error("Message not properly distributed to recipients")
 	}
 
 }
