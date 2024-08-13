5bfac1cc8319f8497bc9d422b7e771ba1f799d54
Matt Scott <matthew.scott3142@gmail.com>
2024-08-13 12:03:49 +0100

    add message tests

--- infra/message_test.go
+++ infra/message_test.go
@@ -1,6 +1,7 @@
 package infra_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/MattSScott/basePlatformSOMAS/infra"
@@ -24,9 +25,9 @@ type IExtendedAgent interface {
 	infra.IAgent[IExtendedAgent]
 	GetAgentField() int
 	GetAllMessages([]IExtendedAgent) []infra.IMessage[IExtendedAgent]
-	HandleMessage1(msg Message1) Message1
-	HandleMessage2(msg Message2) Message2
-	HandleNullMessage(msg NullMessage) NullMessage
+	HandleMessage1(msg Message1)
+	HandleMessage2(msg Message2)
+	HandleNullMessage(msg NullMessage)
 }
 
 type ExtendedAgent struct {
@@ -34,16 +35,19 @@ type ExtendedAgent struct {
 	agentField int
 }
 
-func (agent *ExtendedAgent) HandleMessage1(msg Message1) Message1 {
-	return msg
+func (agent *ExtendedAgent) HandleMessage1(msg Message1) {
+	agent.agentField += msg.messageField1
 }
 
-func (agent *ExtendedAgent) HandleMessage2(msg Message2) Message2 {
-	return msg
+func (agent *ExtendedAgent) HandleMessage2(msg Message2) {
+	agent.agentField += msg.messageField2
+
 }
 
-func (agent *ExtendedAgent) HandleNullMessage(msg NullMessage) NullMessage {
-	return msg
+func (agent *ExtendedAgent) HandleNullMessage(msg NullMessage) {
+	fmt.Println("handled!")
+	agent.agentField = 0
+	fmt.Println(agent.agentField)
 }
 
 func (m1 Message1) InvokeMessageHandler(agent IExtendedAgent) {
@@ -55,6 +59,7 @@ func (m2 Message2) InvokeMessageHandler(agent IExtendedAgent) {
 }
 
 func (nm NullMessage) InvokeMessageHandler(agent IExtendedAgent) {
+	fmt.Println("Handling!")
 	agent.HandleNullMessage(nm)
 }
 
@@ -62,27 +67,19 @@ func (ea *ExtendedAgent) GetAgentField() int {
 	return ea.agentField
 }
 
-func (ea *ExtendedAgent) CreateMessage1() Message1 {
+func (ea *ExtendedAgent) GetMessage1() Message1 {
 	return Message1{
-		infra.BaseMessage{},
+		infra.CreateBaseMessage(ea.GetID()),
 		5,
 	}
 }
 
-// func (ea *ExtendedAgent) HandleMessage1(msg Message1) {
-// 	ea.agentField += msg.messageField1
-// }
-
-func (ea *ExtendedAgent) CreateMessage2() Message2 {
+func (ea *ExtendedAgent) GetMessage2() Message2 {
 	return Message2{
-		infra.BaseMessage{},
+		infra.CreateBaseMessage(ea.GetID()),
 		10,
 	}
 }
-
-// func (ea *ExtendedAgent) HandleMessage2(msg Message2) {
-// 	ea.agentField += msg.messageField2
-// }
 
 func (ea *ExtendedAgent) GetNullMessage() NullMessage {
 	return NullMessage{
@@ -90,50 +87,57 @@ func (ea *ExtendedAgent) GetNullMessage() NullMessage {
 	}
 }
 
-// func (ea *ExtendedAgent) HandleNullMessage(msg NullMessage) {
-// 	// sender := msg.GetSender()
+func TestMessageCanBeExtended(t *testing.T) {
+	agent1 := &ExtendedAgent{agentField: 0}
+	agent2 := &ExtendedAgent{agentField: 0}
 
-// }
+	msgFromA1 := agent1.GetMessage1()
+	msgFromA1.InvokeMessageHandler(agent2)
 
-// func (ea *ExtendedAgent) GetAllMessages([]IExtendedAgent) []infra.IMessage[IExtendedAgent] {
-// 	msg1 := ea.GetMessage1()
-// 	msg2 := ea.GetMessage2()
-// 	return []infra.IMessage{msg1, msg2}
-// }
-// func TestMessageCanBeExtended(t *testing.T) {
-// 	agent1 := &ExtendedAgent{agentField: 0}
-// 	agent2 := &ExtendedAgent{agentField: 0}
+	msgFromA2 := agent2.GetMessage2()
+	msgFromA2.InvokeMessageHandler(agent1)
 
-// 	msgFromA1 := agent1.GetMessage1()
-// 	msgFromA1.InvokeMessageHandler(agent2)
+	if agent1.agentField != 10 {
+		t.Error("Message 2 not properly handled")
+	}
 
-// 	msgFromA2 := agent2.GetMessage2()
-// 	msgFromA2.InvokeMessageHandler(agent1)
+	if agent2.agentField != 5 {
+		t.Error("Message 1 not properly handled")
+	}
+}
+func TestSingleMessageGetsHandled(t *testing.T) {
 
-// 	if agent1.agentField != 10 {
-// 		t.Error("Message 2 not properly handled")
-// 	}
+	a1 := &ExtendedAgent{agentField: 5}
+	a2 := &ExtendedAgent{agentField: 10}
+	a3 := &ExtendedAgent{agentField: 15}
 
-// 	if agent2.agentField != 5 {
-// 		t.Error("Message 1 not properly handled")
-// 	}
-// }
-
-func TestMessageSender(t *testing.T) {
-
-	a1 := ExtendedAgent{agentField: 5}
-	a2 := ExtendedAgent{agentField: 10}
-	a3 := ExtendedAgent{agentField: 15}
-
-	// nullMsg := a1.GetNullMessage([]IExtendedAgent{a2, a3})
 	nullMsg := a1.GetNullMessage()
 
-	for _, recip := range []*ExtendedAgent{&a2, &a3} {
+	for _, recip := range []IExtendedAgent{a2, a3} {
 		nullMsg.InvokeMessageHandler(recip)
 	}
 
-	if a1.GetAgentField() != a2.GetAgentField() || a3.GetAgentField() != a1.GetAgentField() {
+	fmt.Println(a2.agentField)
+
+	if a2.GetAgentField() != 0 || a3.GetAgentField() != 0 {
 		t.Error("Message not properly distributed to recipients")
 	}
+}
 
+func TestMultipleMessagesGetHandled(t *testing.T) {
+
+	a1 := &ExtendedAgent{agentField: 0}
+	a2 := &ExtendedAgent{agentField: 0}
+
+	allMessages := []infra.IMessage[IExtendedAgent]{a1.GetMessage1(), a1.GetMessage1(), a1.GetMessage2()}
+
+	for _, msg := range allMessages {
+		msg.InvokeMessageHandler(a2)
+	}
+
+	fmt.Println(a2.agentField)
+
+	if a2.GetAgentField() != 20 {
+		t.Error("Agents unable to handle multiple message types")
+	}
 }
